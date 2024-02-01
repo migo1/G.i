e6d537291dc71b6249ac0a40129da6a183f299da
migo1 <ertmigot@gmail.com>
2024-02-01 20:15:52 +0300

    setup-context-files-and-schemas-with-relations

---
+++ test/support/fixtures/comments_fixtures.ex
@@ -0,0 +1,20 @@
+defmodule Gi.CommentsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Gi.Comments` context.
+  """
+
+  @doc """
+  Generate a comment.
+  """
+  def comment_fixture(attrs \\ %{}) do
+    {:ok, comment} =
+      attrs
+      |> Enum.into(%{
+        markup_text: "some markup_text"
+      })
+      |> Gi.Comments.create_comment()
+
+    comment
+  end
+end
